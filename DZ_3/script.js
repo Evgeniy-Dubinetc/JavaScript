914b72839cf66b538b6a07ed8c04103004d78512
Евгений Дубинец <edubinetc2@mail.ru>
2022-06-13 18:30:46 +0300

    HW_3

---
+++ DZ_3/script.js
@@ -0,0 +1,81 @@
+
+// Task_1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
+
+let nums = 0;
+while (nums <= 100) {
+    let check = true;
+    for (let i = 2; i < nums; i++) {
+        if (nums % i === 0) {
+            check = false;
+            break;
+        }
+    }
+    if (check) console.log(nums);
+    nums++;
+}
+
+// Task_2. С этого урока начинаем работать с функционалом интернет-магазина. 
+// Предположим, есть сущность корзины. 
+// Нужно реализовать функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров.
+
+let basket = [
+    {
+        product: "pen",
+        price: getNumber(50, 100)
+    },
+    {
+        product: "pencil",
+        price: getNumber(50, 100)
+    },
+    {
+        product: "file",
+        price: getNumber(50, 100)
+    },
+    {
+        product: "paper",
+        price: getNumber(50, 100)
+    }
+];
+let basketPrice = 0;
+for (let prod of basket) {
+    basketPrice += prod.price;
+    console.log("Товар " + prod.product + " стоит: " + prod.price);
+}
+
+console.log("Стоимость корзины: " + basketPrice + " у.е.");
+
+// Task_3. Товары в корзине хранятся в массиве. 
+// Задачи:
+// a) Организовать такой массив для хранения товаров в корзине;
+// b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
+
+function countBasketPrice(basket) {
+    let funBasketPrice = 0;
+    for (let prod of basket) {
+        funBasketPrice += prod.price;
+    }
+    return funBasketPrice;
+}
+
+console.log("Стоимость корзины: " + countBasketPrice(basket) + " у.е.");
+
+// Task_4.*Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. 
+// Выглядеть это должно так: for(…){// здесь пусто}
+
+for (let i = 0; i < 10; console.log(i++)) { }
+
+// ------------------------------------------------------------------------------------------------------------
+
+// Task_5 *Нарисовать пирамиду с помощью console.log, как показано на рисунке, 
+// только у вашей пирамиды должно быть 20 рядов, а не 5:
+// x
+// xx
+// xxx
+// xxxx
+// xxxxx
+
+let row = 'x';
+for (let i = 0; i < 20; i++) {
+    console.log(row);
+    row += 'x';
+}
